3a50cb6861b768ae5d458865e90e1d7602be8e09
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-11-17 17:10:36 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ class App extends Component {
 	  
 	  
   }
+  
+  
+  createContact(contact){
+	  
+	  ContactsAPI.create(contact).then(contact => {
+		  this.setState(state =>({
+			  contacts: state.contacts.concat([contact])
+		  }))
+	  })
+  }
 	
   render() {
 	  
@@ -43,7 +53,14 @@ class App extends Component {
 			/>
 		)}/>
 		
-		<Route path="/create" component={CreateContact}/>
+		<Route path="/create" render={({history})=>(
+			<CreateContact
+				onCreateContact={(contact)=> {
+					this.createContact(contact)
+					history.push('/')
+				}}
+			/>
+		)}/>
 	  
       </div>
     )
